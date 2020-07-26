2d6d0088fd96309589fb9d59b955b3012a5749c5
Demi <demi1024w@126.com>
2020-07-26 21:57:33 +0800

    add component:Icon

---
+++ src/plugins/components/Icon/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <i :class="classes"></i>
+</template>
+<script>
+import {computed} from 'vue'
+const prefixCls = 'icon';
+export default {
+  name:'Icon',
+  props:{
+    type:{
+      type:String,
+      default:''
+    }
+  },
+  setup(props){
+    const classes = computed(()=>{
+                return [
+                    `${prefixCls}`,
+                    {
+                        [`${prefixCls}-${this.type}`]: this.type !== '',
+                        [`${this.custom}`]: this.custom !== '',
+                    }
+                ];
+            })
+    return {classes}
+  }
+}
+</script>
