e0ddef7236e3052fa16462c88ce74724923d270f
Demi <demi1024w@126.com>
2020-08-02 23:16:48 +0800

    Add secondary directories

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@
 // function resolve(dir){
 //   return path.join(__dirname,dir)
 // }
+const BASE_URL = '/lemon/'
 module.exports={
+  publicPath:BASE_URL,
   devServer: {
     open: true // 配置自动启动浏览器
   }
