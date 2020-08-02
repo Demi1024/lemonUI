9fd9ad03d5acf5443f250c043ac628d07391c389
Demi <demi1024w@126.com>
2020-08-02 23:02:28 +0800

    add switch click event

--- src/components/Docs/DeSwitch.vue
+++ src/components/Docs/DeSwitch.vue
@@ -1,11 +1,21 @@
 <template>
   <article>
     <h2>Swicth 开关</h2>
-    <div><de-switch></de-switch></div>
+    <div>
+      <de-switch :value="test" @input="changeValue"></de-switch>
+    </div>
   </article>
 </template>
-<script>
+<script lang="ts">
+import { ref } from 'vue'
 export default {
-  name:'DeSwicth'
+  name:'DeSwicth',
+  setup(){
+    const test = ref(true)
+    const changeValue = (event)=>{
+      test.value = event
+    }
+    return{test,changeValue}
+  }
 }
 </script>
--- src/plugins/components/Switch/index.vue
+++ src/plugins/components/Switch/index.vue
@@ -1,25 +1,62 @@
 <template>
-  <button class="swicth"><span></span></button>
+  <div :class="classes" @click="toogle"><span></span></div>
 </template>
 
-<script>
+<script lang="ts">
+const prefixCls = 'de-switch'
+import { ref, computed } from 'vue'
   export default {
     name: 'Swicth',
-    data() {
-      return {
-        
+    props:{
+      value:{
+        type:Boolean|| String,
+        default:false
       }
+    },
+    setup(props,context){
+      const status = computed(()=> ref(props.value))
+      const classes = computed(()=>{
+                return [
+                    `${prefixCls}`,
+                    {
+                      [`${prefixCls}-checked`]:props.value
+                    }
+                ]
+            })
+      const toogle = ()=>{
+        context.emit('input',!props.value)
+      }
+      return{status,classes,toogle}
     }
   }
 </script>
 
 <style lang="scss" scoped>
   $height:22px;
-  .swicth{
+  $height2:$height - 4px;
+  .de-switch{
     position: relative;
     width:$height*2;
     height: $height;
-    background: royalblue;
+    background: #cccccc;
     border-radius: $height/2;
+    border:none;
+    &-checked{
+      background: royalblue;
+      > span{
+        left: calc(100% - #{$height2} - 4px);
+      }
+    }
   }
+  span{
+      position: absolute;
+      top:2px;
+      left:2px;
+      display: inline-block;
+      width: $height2;
+      height:$height2;
+      border-radius: $height2/2;
+      background:#ffffff;
+      transition: left 250ms linear;
+    }
 </style>
