b5a192110d0bd0ffa5558d8db2b7b19dae11e1bb
Demi <demi1024w@126.com>
2020-08-16 00:10:05 +0800

    :heavy_plus_sign: add highlight.js

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import LemonUI from './plugins/index'
+import Highlight from './libs/types/highlight';
 
 const app = createApp(App)
 app.use(router)
 app.use(LemonUI)
+app.use(Highlight)
 app.mount('#app')
