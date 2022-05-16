f7db8939ed37d4349d55e097a288f915ea4ba4a2
demi1024 <demi1024w@126.com>
2022-05-16 20:34:16 +0800

    feat: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@
 // function resolve(dir){
 //   return path.join(__dirname,dir)
 // }
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
 const BASE_URL = '/lemon/'
-module.exports={
-  publicPath:BASE_URL,
-  devServer: {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base:BASE_URL,
+  server: {
     open: true // 配置自动启动浏览器
   }
-}
+})
