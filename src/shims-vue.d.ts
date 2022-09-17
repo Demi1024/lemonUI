bc0d45b1633f5d926823aef5a07511b857990b6f
demi1024 <demi1024w@126.com>
2022-09-17 23:42:56 +0800

    feat(logo): add new logo

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,3 +1,9 @@
+declare module '*.svg' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent;
+  export default component;
+}
+
 // 增加对vue文件的声明
 declare module "*.vue" {
   import {ComponentOptions} from 'vue'
