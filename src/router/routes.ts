7bf0a8958e2bd0fdad91ebc523a846858fb08a87
Demi <demi1024w@126.com>
2022-09-21 23:12:58 +0800

    fix(router): fix router components import

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,12 @@
-import Home from '../views/home/index.vue'
-import Docs from '../views/doc/index.vue'
-import GetStart from '../components/doc/get-start.vue'
-import DeIcon from '../components/doc/icon.vue'
-import DeButton from '../components/doc/button.vue'
-import DeInput from '../components/doc/input.vue'
-import DeSelect from '../components/doc/select.vue'
-import DeSwitch from '../components/doc/switch.vue'
-import DeGrid from '../components/doc/grid.vue'
+import Home from '@/views/home/index.vue'
+import Docs from '@/views/doc/index.vue'
+import GetStart from '@/components/doc/get-start.vue'
+import DeIcon from '@/components/doc/icon.vue'
+import DeButton from '@/components/doc/button.vue'
+import DeInput from '@/components/doc/input.vue'
+import DeSelect from '@/components/doc/select.vue'
+import DeSwitch from '@/components/doc/switch.vue'
+import DeGrid from '@/components/doc/grid.vue'
 export default [
   {
     path:'/',
