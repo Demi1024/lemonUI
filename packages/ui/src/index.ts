1bffdab26dc0e07f6a4924d8d17ceb3c72d75aed
Demi <Demi1024w@126.com>
2024-06-29 23:06:39 +0800

    feat(ui): update ui installer

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,9 +6,26 @@
  * @desc [LemonUI]
  */
 
-export { default as Switch } from "./components/Switch/index.vue";
-export { default as Input } from "./components/Input/index.vue";
-export { default as Button } from "./components/Button/index.vue";
-export { default as Col } from "./components/Col/index.vue";
-export { default as Row } from "./components/Row/index.vue";
-export { default as Icon } from "./components/Icon/index.vue";
+import Switch from "./components/Switch/index.vue";
+import Input from "./components/Input/index.vue";
+import Button from "./components/Button/index.vue";
+import Col from "./components/Col/index.vue";
+import Row from "./components/Row/index.vue";
+import Icon from "./components/Icon/index.vue";
+import Container from './components/Container/index.vue';
+import { App } from 'vue'
+import './styles/index.scss'
+
+const Installer = {
+  install(app: App) {
+    app.component('le-button', Button);
+    app.component('le-col', Col);
+    app.component('le-container', Container);
+    app.component('le-icon', Icon);
+    app.component('le-input', Input);
+    app.component('le-row', Row);
+    app.component('le-switch', Switch);
+  }
+}
+
+export default Installer;
